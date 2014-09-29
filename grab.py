f1b964136f2bdd5122e98abc021eb22941449e05
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2014-09-29 00:03:49 -0400

    Updated and working python file to grab and plot data

--- grab.py
+++ grab.py
@@ -3,27 +3,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from mpl_toolkits.mplot3d import Axes3D
+import math
+
+x_size = 40
+y_size = 40
+totalData_size = x_size * y_size
+data = [0] * totalData_size
+max_data = 0
+expectedDistance = []
+servoAngles = []
+
+x_scatter = []
+y_scatter = []
+z_scatter = []
+
+x_final = []
+y_final = []
+z_final = []
 
 ser = serial.Serial('/dev/tty.usbmodemfd121',9600)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-data = [0] * 900
-max_data = 0
 
-for x in range(900):
+for x in range(totalData_size):
+	servoAngles.append(( float(ser.readline())*math.pi/180.0, float(ser.readline())*math.pi/180.0 ))
 	data[x] = float(ser.readline())
 	if data[x] > max_data:
 		max_data = data[x]
-	print data[x]
+	print data[x], servoAngles[x]
 
-for i in range(900):
+for i in range(totalData_size):
 	data[i] = data[i] / max_data
 
-y_scatter = range(30) * 30
-x_scatter = range(900)
-x_scatter = [n/30 for n in x_scatter]
+a = 372.2
+b = -3.962
+c = 99.51
+d = -0.6113
 
-ax.scatter(x_scatter, y_scatter, data)
+for i in data:
+	expectedDistance.append(a * math.exp(b * i) + c * math.exp(d * i))
+
+x_scatter = range(totalData_size)
+x_scatter = [n/x_size for n in x_scatter]
+y_scatter = range(y_size) * x_size
+
+print expectedDistance
+
+for i in range(totalData_size):
+	if expectedDistance[i] < 100:
+		x_final.append(x_scatter[i])
+		y_final.append(y_scatter[i])
+		z_final.append(expectedDistance[i])
+
+# for i in range(totalData_size):
+# 	x_scatter.append(expectedDistance[i] * np.cos(servoAngles[i][0]) * np.sin(servoAngles[i][1]))
+# 	y_scatter.append(expectedDistance[i] * np.sin(servoAngles[i][0]) * np.sin(servoAngles[i][1]))
+# 	z_scatter.append(expectedDistance[i] * np.cos(servoAngles[i][1]))
+
+ax.scatter(x_final, y_final, z_final)
 plt.show()
