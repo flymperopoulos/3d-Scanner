9a533f7197b86ec28a695ef49c5fe5aca7be2620
jwoo1123 <Jay.Woo@students.olin.edu>
2014-09-30 02:49:18 -0400

    Update Sensor3DArduino.ino

--- Sensor3DArduino/Sensor3DArduino.ino
+++ Sensor3DArduino/Sensor3DArduino.ino
@@ -1,110 +1,83 @@
-// Includes the Servo.h library.
 #include <Servo.h> 
 
-// Creation of servo objects to control the servos in vertical and horizontal directions.
 Servo servoHorizontal;   
 Servo servoVertical;
 
-// Definition of servos' and sensor's pin.
 const int sensorPin = A1;
 const int servoPinX = 13;
 const int servoPinY = 9;
 
-// Stores last time LED was updated.
+// Stores the time to implement delays
 long time = 0;    
 
-// Interval between events.
+// Time interval between each sensor reading
 int sensingInterval = 120; 
 
-// Initializes counter.
 int counter;
 float data_sum;
-
-// Definition of servo angles.
 int servoAngleX;
 int servoAngleY;
  
 void setup() 
 {
-  // Definition of sensor input pin.
   pinMode(sensorPin, INPUT);  
   
-  // Attaches the servos on pin 9 and 13 to the servo objects.
   servoHorizontal.attach(servoPinX);  
   servoVertical.attach(servoPinY);  
   
-  // Opens serial port, sets data rate to 9600 bps
   Serial.begin(9600);
 } 
  
 // Initializes the loop() method.
 void loop() {
   
-  // Writes positions of Horizontal and Vertical servos.
+  // Initializes the servo positions
   servoHorizontal.write(50);
   servoVertical.write(70);
   
-  // Declares delay.
   delay(2000);
   
-  // Assign values to servo angles.
+  // Keeps track of servo angles to print to the serial
   servoAngleX = -20;
   servoAngleY = -20;
   
-   // External "for-loop" for horizontal servo sweep. 
-   for (int currentXPos = 50; currentXPos < 90; currentXPos++){
-     
-     // Writes horizontal servo position.
-     servoHorizontal.write(currentXPos);
-     
-     // Writes vertical servo position.
-     servoVertical.write(70);
-     // Declaration of delay and time variable.
-     delay(200);
-     time = millis();
-     
-     // Internal "for-loop" for verticak servo sweep.
-     for (int currentYPos = 70; currentYPos < 110; currentYPos++){
-       
-       // Writes position of vertical servo
-       servoVertical.write(currentYPos);
-       
-       // Assigns counter to integer 1.
-       counter = 1;
-       
-       // Initializes data_sum variable to zero.
-       data_sum = 0.0;
-       
-       // Prints angle measurements on both planes.
-       Serial.println(servoAngleX);
-       Serial.println(servoAngleY);
-       
-       // Increments vertical servo angle.
-       servoAngleY++;
-       
-       // while loop without delay that checks difference between current and previous time data were read from the sensorPin
-       while (millis() - time < sensingInterval) {
-         
-         // Updates data_sum variable, based on analogRead(sensorPin).
-         data_sum += analogRead(sensorPin) * 5.0/1023.0;
-         
-         // Increments counter.
-         counter++;
-       }
-       // Returns the number of milliseconds since the Arduino began running the current program.
-       time = millis();
-       
-       // Prints the average value of the data collected.
-       Serial.println(data_sum / counter);
-     }
-     
-     // Increments horizontal angle.
-     servoAngleX++;
-     
-     // Definition of vertical angle to -20.
-     servoAngleY = -20;
-   }
-   
-   // "While-loop" that stops program from servos moving.
-   while (1){}
+  // Sweeps through the data
+  for (int currentXPos = 50; currentXPos < 90; currentXPos++){
+    servoHorizontal.write(currentXPos);
+    servoVertical.write(70); // Resets the vertical servo
+    
+    // Declaration of delay and time variable.
+    delay(200);
+    time = millis();
+    
+    // Vertical sweep
+    for (int currentYPos = 70; currentYPos < 110; currentYPos++){
+      servoVertical.write(currentYPos);
+      
+      // Prints the servo angles to the serial
+      Serial.println(servoAngleX);
+      Serial.println(servoAngleY);
+      servoAngleY++;
+      
+      // Averages the data collected over the sensingInterval
+      counter = 1;
+      data_sum = 0.0;
+      
+      while (millis() - time < sensingInterval) {
+        counter++;
+        data_sum += analogRead(sensorPin) * 5.0/1023.0;
+      }
+      
+      time = millis();
+
+      // Prints the average value of the data collected
+      Serial.println(data_sum / counter);
+    }
+    
+    servoAngleX++;
+    servoAngleY = -20;
+  }
+  
+  // Halts everything when the scan is done
+  while (1){}
 }
